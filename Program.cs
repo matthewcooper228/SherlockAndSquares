9cc9310cb8280d8adcada8bf44663ac0e40b1891
matthewcooper228 <matthewcooper228@gmail.com>
2022-06-21 07:58:40 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+int squares(int a, int b)
+{
+    return (int)Math.Sqrt(b) - (int)Math.Sqrt(a);
+}
+int a = 1;
+int b = 1000000000;
+Console.WriteLine("There are " + squares(a,b) + " perfect squares from " + a + " to " + b + ".");
